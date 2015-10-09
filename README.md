ed8ae8ba0b38eb8093978a7f92bd048730946551
Andrew Odewahn <odewahn@oreilly.com>
2015-10-09 16:49:05 -0400

    more work

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 
 * https://github.com/andlabs/ui  -- UI client library
+* https://godoc.org/github.com/andlabs/ui --- Godocs for the UI library
 * https://github.com/samalba/dockerclient  -- Docker client clibrary documentation
+* http://www.gtk.org/features.php
+
+
+```
+docker run -d  \
+   -p 8888 \
+   -P \
+   --name odewahnTest \
+   --hostname hubba.i3.odewahn.com \
+   ipython/scipystack    \
+   /bin/sh -c 'ipython notebook --ip=0.0.0.0 --no-browser'
+```
--- main.go
+++ main.go
@@ -4,14 +4,37 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
+	"github.com/andlabs/ui"
 	"github.com/joho/godotenv"
 	"github.com/odewahn/swarm-manager/manager"
 	"github.com/samalba/dockerclient"
 )
 
-func main() {
+var (
+	w      ui.Window
+	docker *dockerclient.DockerClient
+)
 
+func gui() {
+	connect()
+	b := ui.NewButton("Click me")
+	stack := ui.NewVerticalStack(b)
+
+	b.OnClicked(func() {
+		ps()
+	})
+
+	w = ui.NewWindow("Window", 400, 500, stack)
+	w.OnClosing(func() bool {
+		ui.Stop()
+		return true
+	})
+	w.Show()
+}
+
+func connect() {
 	// Load the environment variables we need
 	err := godotenv.Load()
 	if err != nil {
@@ -23,12 +46,15 @@ func main() {
 		log.Fatal("Could not create TLS certificate.")
 	}
 	// Setup the docker host
-	docker, err := dockerclient.NewDockerClient(os.Getenv("DOCKER_HOST"), tlsConfig)
+	docker, err = dockerclient.NewDockerClient(os.Getenv("DOCKER_HOST"), tlsConfig)
 	if err != nil {
 		log.Fatal("Error initializing docker: ", err)
 	}
 	log.Println("Swarm connection inialized", docker)
 
+}
+
+func ps() {
 	// Get only running containers
 	containers, err := docker.ListContainers(false, false, "")
 	if err != nil {
@@ -36,8 +62,23 @@ func main() {
 	}
 	for _, c := range containers {
 		//log.Println(c.Id)
-		//container, _ := docker.InspectContainer(c.Id)
-		fmt.Printf("%s \t %s\n", c.Image, c.Names[0])
+		container, _ := docker.InspectContainer(c.Id)
+		fmt.Printf("%s \t %s \t %s\n",
+			c.Image,
+			container.Config.Hostname+"."+container.Config.Domainname,
+			strings.Split(c.Names[0], "/")[2])
+	}
+}
+
+func main() {
+
+	// This runs the code that displays our GUI.
+	// All code that interfaces with package ui (except event handlers) must be run from within a ui.Do() call.
+	go ui.Do(gui)
+
+	err := ui.Go()
+	if err != nil {
+		log.Print(err)
 	}
 
 }
